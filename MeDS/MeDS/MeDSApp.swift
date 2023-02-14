ecaf7a552c9fa10807cebe2658acbe33d2dd673b
rparikh10 <78388031+rparikh10@users.noreply.github.com>
2023-02-14 11:53:52 -0500

    interface 1 complete

---
+++ MeDS/MeDS/MeDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeDSApp.swift
+//  MeDS
+//
+//  Created by Rushil Parikh New on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MeDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
